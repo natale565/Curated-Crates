0dbe6f047791b720bac1eeec93e9685eeb8da5b4
scottreynolds6294@gmail.com <scottreynolds6294@gmail.com>
2024-11-01 14:57:24 -0400

    updated login and signup jsx

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -1,11 +1,37 @@
 import { Avatar, Box, TextField, Checkbox, Container, Paper, Typography, FormControlLabel, Button,  } from '@mui/material';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { LOGIN } from '../utils/mutations';
+import Auth from '../utils/auth';
 // import { Link as RouterLink } from 'react-router-dom';
 
-const SignIn = () => {
-    const handleSubmit = () => console.log('SignIn');
-    return (
-        <Container maxwidth='xs'>
+//eslint-disable-next-line
+function Login(props) {
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const mutationResponse = await login({
+        variables: { email: formState.email, password: formState.password },
+      }); 
+      const token = mutationResponse.data.login.token;
+      Auth.login(token);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+    const handleChange = (event) => {
+      const { name, value } = event.target;
+      setFormState({
+        ...formState,
+        [name]: value,
+      });
+    };
+  return (
+        <Container maxWidth='xs'>
           <Paper elevation={10} sx= {{ marginTop: 8, padding: 2}}>
             <Avatar 
               sx={{
@@ -22,10 +48,27 @@ const SignIn = () => {
             </Typography>
             <Box 
             component='form' 
-            onSubmit={handleSubmit} 
+            onSubmit={handleFormSubmit} 
             noValidate sx={{mt: 1}}>
-                <TextField placeholder='Enter username' fullWidth required autoFocus sx={{mb: 2}}/>
-                <TextField placeholder='Enter password' fullWidth required type='password' />
+                 <TextField
+                        name='email'
+                        placeholder='Enter email'
+                        fullWidth
+                        required
+                        type='email'
+                        value={formState.email}
+                        onChange={handleChange}
+                        sx={{ mb: 2 }}
+                    />
+                    <TextField
+                        name='password'
+                        placeholder='Enter password'
+                        fullWidth
+                        required
+                        type='password'
+                        value={formState.password}
+                        onChange={handleChange}
+                    />
                 <FormControlLabel control={<Checkbox value='remember' color='primary' />} label='Remember me'/>
                 <Button type='submit' fullWidth variant='contained' sx={{mt: 1}}>Sign In</Button>
             </Box>
@@ -42,4 +85,4 @@ const SignIn = () => {
     )
 }
 
-export default SignIn;
+export default Login;
