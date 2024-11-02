6c0c31256f8343aaecfa35fd838e0994e78e1164
Chris Natale <natale565@icloud.com>
2024-11-02 10:31:54 -0400

    created components for current/past subs and added to the dash page

---
+++ client/src/components/PastOrders.jsx
@@ -0,0 +1,19 @@
+import Box from '@mui/material/Box';
+
+function PastOrders() { 
+    return(
+        <>
+            <h2>Past Orders</h2>
+            <Box component="div" sx={{ display: 'flex', gap: 2 }}>
+                <Box component="div" sx={{ flex: 1, minHeight: '150px' }}>
+                    <h2 style={{ textAlign: 'center' }}>Step 1</h2>
+                    <Box component="section" sx={{ p: 2, border: '1px solid grey', height: '100%' }}>
+                        Choose Your Box – Begin by exploring our three unique box options. Each is thoughtfully crafted to bring you joy, discovery, and quality.
+                    </Box>
+                </Box>
+            </Box>
+        </>
+    )
+}
+
+export default PastOrders;
--- client/src/pages/Dash.jsx
+++ client/src/pages/Dash.jsx
@@ -1,7 +1,12 @@
+import PastOrders from '../components/PastOrders';
+import CurrentSub from '../components/CurrentSub';
+
 function Dash(){
     return (
         <div>
-            <h1>Dashboard</h1>
+            <h1>Welcome to the Dashboard</h1>
+            <PastOrders />
+            <CurrentSub />
         </div>
     )
 }
