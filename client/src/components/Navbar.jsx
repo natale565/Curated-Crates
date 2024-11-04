d87a2f9a7a2cd2cbaa189fb304c1268e71bd5f7f
Chris Natale <natale565@icloud.com>
2024-11-04 18:44:33 -0500

    style file, changed navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,49 +1,13 @@
-// // eslint-disable-next-line react/prop-types
-// function NavBar ({currentPage, handlePageChange}){
-//     return (
-//         <>
-//         <nav>
-//             <ul>
-//                 <li>
-//                     <a href="#HomePage"
-//                     onClick={() => handlePageChange("HomePage")}
-//                     className={currentPage === "HomePage" ? "nav-link active" :
-//                     "nav-link"}
-//                     >Home</a>
-//                 </li>
-//                 <li>
-//                     <a href="#HowItWorks" 
-//                     onClick={() => handlePageChange("HowItWorks")} 
-//                     className={currentPage === "HowItWorks" ? "nav-link active" : 
-//                     "nav-link"}
-//                     >How It Works</a>
-//                 </li>
-//                 <li>
-//                     <a href="#SignUp" onClick={() => handlePageChange("SignUp")} className={currentPage === "SignUp" ? "nav-link active" :
-//                      "nav-link"}
-//                      >Sign Up</a>
-//                 </li>
-//                 <li>
-//                     <a href="#signIn" onClick={() => handlePageChange("SignIn")} className={currentPage === "SignIn" ? "nav-link active" : 
-//                     "nav-link"}
-//                     >Sign In</a>
-//                 </li>
-//             </ul>
-//         </nav>
-//         </>
-//     );
-// }
 
-// export default NavBar;
 import React from 'react';
 import { AppBar, Toolbar, Typography, Stack, Button } from '@mui/material';
-import { FaShoppingCart } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import './style.css'
 
 // eslint-disable-next-line react/prop-types
 function NavBar({ isAuthenticated, onLogout }) {
     return (
-        <AppBar position='static'>
+        <AppBar position='static' className='Nav'>
             <Toolbar>
                 <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
                     Curated Crates
